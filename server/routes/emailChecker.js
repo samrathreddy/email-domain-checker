c364d38d53d1bc206dda876b290b9582b2591ba0
Samrath <samrathreddy04@gmail.com>
2025-03-06 13:51:28 +0530

    Vercel Update

--- server/routes/emailChecker.js
+++ server/routes/emailChecker.js
@@ -4,5 +4,8 @@ import checkEmail from '../controllers/emailController.js';
 const router = express.Router();
 
 router.get('/check-email', checkEmail);
+router.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Server is running' });
+});
 
 export default router;
